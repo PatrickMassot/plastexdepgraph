787d8df2dce2b81aab7eb767d0b1eb8c22863ce3
Patrick Massot <patrickmassot@free.fr>
2024-01-30 17:54:22 -0500

    Improve packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="plastexdepgraph",
-    version="0.0.1",
-    author="Patrick Massot",
-    description="Dependency graph plasTeX plugin.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    package_data={'plastexdepgraph': ['static/*', 'templates/*', 'Packages/*',
-                                    'Packages/renderer_templates/*']},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent"],
-    python_requires='>=3.7',
-    install_requires=['plasTeX>=3.0', 'pygraphviz']
-)
+if __name__ == "__main__":
+    setup()
+
